9d407b5ec62cefdba4f99ab87c10aa91f3ad3d43
devyanshuk <devkoirala10@gmail.com>
2022-03-25 02:49:35 +0100

    added predictions for test and validation data

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from Dataset.labels import *
 from Dataset.dataset import Dataset
 from NeuralNetwork.NN import NeuralNetwork
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 if __name__ == "__main__":
     seed = 42
     train = Dataset(name=TRAIN_DATA, isTarget=False)
     t_target = Dataset(name=TRAIN_TARGET, isTarget=True)
+    test = Dataset(name=TEST_DATA, isTarget=False)
+    test_target = Dataset(name=TEST_TARGET, isTarget=True)
 
     train_data, validation_data, train_target, validation_target = \
         train_test_split(train.data, t_target.data, test_size=10000, random_state=seed)
@@ -20,6 +23,8 @@ if __name__ == "__main__":
     log.info(f"validation data shape = {validation_data.shape}")
     log.info(f"train target shape = {train_target.shape}")
     log.info(f"validation target shape = {validation_target.shape}")
-    nn = NeuralNetwork(classes=10, seed = 42)
+    nn = NeuralNetwork(classes=10, seed = 42, epochs=20)
     nn.fit(train_data, train_target)
+    log.info(f"validation dataset score = {(100 * accuracy_score(validation_target, nn.predict(validation_data))):.2f}%")
+    log.info(f"test dataset score = {(100 * accuracy_score(test_target.data, nn.predict(test.data))):.2f}%")
 
